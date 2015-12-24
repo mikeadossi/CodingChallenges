884cbd681e3010bd1f2e434356bb4e56f89b424d
ANnamdi <mikeadossi@gmail.com>
2015-12-24 11:37:28 -0600

    Pizza party

---
+++ exercise8.js
@@ -0,0 +1,23 @@
+'use strict'
+var prompt = require('prompt');
+prompt.start();
+
+console.log('We\'ll be evenly divying up a pizza, complete following entries:')
+prompt.get(['number of people eating','number of pizzas','even number of slices per pizza'],function(err, result){
+	var numOfPpl = result['number of people eating']
+	var numOfPizzas = result['number of pizzas']
+	var numOfSlices = result['even number of slices per pizza']
+	var totalSlices = numOfPizzas * numOfSlices
+	var pizzaPerPerson = totalSlices / numOfPpl
+
+	console.log('There are '+ numOfppl +' people with '+ numOfPizzas +' pizzas.')
+	evenSlices(numOfSlices);
+	console.log('Each person gets '+ numOfSlices +' pieces of pizza.')
+})
+
+
+function evenSlices(x){
+	if(x%2 > 0){ // we use modulus here!
+		console.log('your need an even number of slices')
+	}
+}
