931617533bccb48293f61b51bc0446c8efe88aeb
ANnamdi <mikeadossi@gmail.com>
2015-12-26 18:31:48 -0600

    Password validation

---
+++ exercise15.js
@@ -0,0 +1,20 @@
+'use strict'
+var prompt = require('prompt')
+prompt.start();
+
+console.log('Password Validation program, hint username = mike, password = openSesame')
+prompt.get(['username','password'],function(err, result){
+	var username = result.username
+	var password = result.password
+	validate();
+
+
+function validate(){
+	if(username == 'alibaba' && password == 'openSesame'){
+		console.log('Access granted!')
+	} else{
+		console.log('username or password is incorrect')
+	}
+ }
+
+})
