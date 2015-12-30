645a4043aad6ee17380b9211784ff0d28aa24055
ANnamdi <mikeadossi@gmail.com>
2015-12-30 14:22:23 -0600

    Sales Tax Calculator

---
+++ exercise20.js
@@ -0,0 +1,31 @@
+// keep geting NaN
+'use strict'
+
+var prompt = require('prompt');
+prompt.start();
+
+console.log('Multistate Sales Tax Calcuilator')
+prompt.get(['orderAmt','enter abbreviated state'],function(err,result){
+	var orderAmt = parseInt(result.orderAmt);
+	var state = result['enter abbreviated state'];
+	var taxAmt;
+	checkState();
+	var amtAfterTax = orderAmt * taxAmt
+	
+
+	console.log('Your given order amount was '+orderAmt)
+	console.log('Your tax amount in '+state+' is '+taxAmt.toFixed(2))
+	console.log('Your after tax amount is '+amtAfterTax)
+
+	function checkState(){
+		if(state == 'TX'){
+			taxAmt = orderAmt * 0.0625
+		}
+		if(state == 'CA'){
+			taxAmt = orderAmt * 0.075
+		}
+		if(state == 'NY'){
+			taxAmt = orderAmt * 0.04
+		}
+	}
+})
