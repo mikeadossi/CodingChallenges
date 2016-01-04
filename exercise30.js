8918ac790ac2ef8388c2d0242bc328e43302f9c5
ANnamdi <mikeadossi@gmail.com>
2016-01-03 19:59:12 -0600

    Multiplication table

---
+++ exercise30.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var prompt = require('prompt-sync').prompt;
+console.log('Multiplication Table Program')
+
+for(var i = 0; i<=12; i++){
+
+	for(var y = 0; y<=12; y++){
+		console.log(i*y)
+	}
+}
