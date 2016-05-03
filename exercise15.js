b3dfdea47bbcd23203371227e5607765443bcf99
mikeadossi <mikeadossi@gmail.com>
2016-05-03 12:34:34 -0500

    edited string

--- exercise15.js
+++ exercise15.js
@@ -2,7 +2,7 @@
 var prompt = require('prompt')
 prompt.start();
 
-console.log('Password Validation program, hint username = mike, password = openSesame')
+console.log('Password Validation program, hint username = alibaba, password = openSesame')
 prompt.get(['username','password'],function(err, result){
 	var username = result.username
 	var password = result.password
