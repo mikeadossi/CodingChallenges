10f9eb88016d0a99d3c8f6586924db8868ec328d
mikeadossi <mikeadossi@gmail.com>
2016-05-03 13:34:50 -0500

    added code to check if entries were the same

--- exercise22.js
+++ exercise22.js
@@ -11,10 +11,15 @@ prompt.get(['number1','number2','number3'], function(err, result){
 	arrayOfNum[1] = parseInt(result.number2)
 	arrayOfNum[2] = parseInt(result.number3)
 	var largest;
-	getLargest();
-	console.log('The largest number given from you list is: '+largest)
+	checkGreatest()
+	console.log('The largest number given from your list is: '+largest)
 
-	function getLargest(){
-		largest = Math.max.apply(null, arrayOfNum); // stackoverflow
+	function checkGreatest(){
+		if(arrayOfNum[0] == arrayOfNum[1] && arrayOfNum[0] == arrayOfNum[2]){
+			console.log('All numbers are equal, none is greater than the other')
+			process.exit()
+		} else {
+			largest = Math.max.apply(null, arrayOfNum);
+		}
 	}
 })
